590260b273520cb92a52029fc601e3463ef41d84
Cesar <cesar.sanchez.gomez@hotmail.com>
2026-04-02 19:41:23 -0600

    build: migrate to shadowJar and update API version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("com.gradleup.shadow") version "9.1.0"
 }
 
 group = "com.cesarcosmico"
@@ -26,7 +27,11 @@ repositories {
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.21.11-R0.1-SNAPSHOT")
     compileOnly("com.github.MilkBowl:VaultAPI:1.7.1")
-    compileOnly("com.github.BreweryTeam:TheBrewingProject:feat~expose-config-to-api-SNAPSHOT")
+    compileOnly("com.github.BreweryTeam:TheBrewingProject:v3.0.0-beta.0")
+
+    implementation("com.zaxxer:HikariCP:7.0.2")
+    implementation("com.mysql:mysql-connector-j:8.0.33")
+    implementation("org.mariadb.jdbc:mariadb-java-client:3.5.7")
 
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
@@ -35,4 +40,14 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+tasks.withType<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar> {
+    archiveClassifier.set("")
+
+    relocate("com.zaxxer.hikari", "com.cesarcosmico.brewmarket.lib.hikari")
+    relocate("com.mysql", "com.cesarcosmico.brewmarket.lib.mysql")
+    relocate("org.mariadb", "com.cesarcosmico.brewmarket.lib.mariadb")
+
+    mergeServiceFiles()
 }
